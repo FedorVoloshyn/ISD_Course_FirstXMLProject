1e17e394f26881bfd44f01fa00f447fd3258cc3e
FedorVoloshyn <tikey.gm@gmail.com>
2016-07-22 13:05:33 +0300

    FirstCommit

---
+++ FirstXMLProject/FirstXMLProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace FirstXMLProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument myFirstXmlDoc = new XmlDocument();
+            myFirstXmlDoc.Load("..\\..\\test.xml");
+            XmlElement root = myFirstXmlDoc.DocumentElement;
+            foreach (XmlNode element in root)
+            {
+                foreach (XmlNode childNodes in element)
+                {
+                    Console.WriteLine(childNodes.InnerText);
+                }
+            }
+        }
+    }
+}
